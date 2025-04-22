8e22d850d727e9ee5a84ad25fa8496a265d12e9b
LakshayTrehan77 <133641907+LakshayTrehan77@users.noreply.github.com>
2025-04-22 21:56:17 +0530

    Update main.py

--- SHL-Assessment-Recommendation-System-main/main.py
+++ SHL-Assessment-Recommendation-System-main/main.py
@@ -1,8 +1,8 @@
 import os
 import subprocess
 
-# Run vector_store.py and Django server in one terminal
+# Run vector_store.py
 subprocess.run(["python", "vector_store.py"])
+# Change directory to rag_api and run Django server on 0.0.0.0:8080
 os.chdir("rag_api")
-subprocess.run(["python", "manage.py", "runserver" , "--noreload"])
-
+subprocess.run(["python", "manage.py", "runserver", "0.0.0.0:8080", "--noreload"])
